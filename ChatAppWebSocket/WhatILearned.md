caa87cf27cf9a422ad34913958b6b44f13ae5246
code_with_bot <programming.with.yadu@gmail.com>
2025-06-26 01:30:41 +0530

    Third task

---
+++ ChatAppWebSocket/WhatILearned.md
@@ -0,0 +1,6 @@
+Here is the things in short term i learned from this project.
+
+Polling -: `Polling` is a technique used in chat applications where the client repeatedly sends requests to the server at regular intervals (e.g., every second) to check for new messages or updates.
+
+But in this process load is happening due to free reeuests. Due to this we use `Web-Sockets`.
+
---
+++ ChatAppWebSocket/index.js
@@ -0,0 +1,35 @@
+const http= require("http");
+const express = require("express");
+const path= require('path');
+const {Server} = require("socket.io");
+const { isKeyObject } = require("util/types");
+const { log } = require("console");
+
+
+const app= express();
+const server= http.createServer(app);
+const io = new Server(server)
+
+
+//socket io
+
+io.on("connection", (socket) =>{
+
+    socket.on('u_message', (message)=>{
+
+        console.log("Message:",message);
+        io.emit("message", message)
+        
+    })
+    
+})
+
+
+app.use(express.static(path.resolve('./public')))
+
+app.get("/", (req, res) =>{
+
+    return res.sendFile("./Public/index.html")
+})
+
+server.listen(9000, ()=> console.log(`Server started at http://localhost:9000`))
